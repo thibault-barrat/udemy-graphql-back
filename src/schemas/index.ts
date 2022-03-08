339ed68fde2640992e175fa162122df0acfbb8c9
thibault-barrat <contact@thibault-barrat.com>
2022-03-08 10:09:00 +0100

    add graphql queries

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,18 +3,49 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
+  GraphQLFloat,
 } from 'graphql';
+import { Product } from '../models/product';
 
-export const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'Hello GraphQL!';
-        },
+const ProductType = new GraphQLObjectType({
+  name: 'Product',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    category: { type: GraphQLString },
+    filter: { type: GraphQLString },
+    price: { type: GraphQLFloat },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve() {
+        return 'Hello GraphQL!';
       },
     },
-  }),
+    products: {
+      type: new GraphQLList(ProductType),
+      resolve(parent, args) {
+        return Product.find();
+      }
+    },
+    productsByCategory: {
+      type: new GraphQLList(ProductType),
+      args: {
+        category: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return Product.find({ category: args.category });
+      }
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
 });
